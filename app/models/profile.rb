aa5678e7597e88ce9383630c9669af2366bd5c81
Hannah Bourke <hannah25@gmail.com>
2017-11-08 10:38:33 +1100

    Upate address - profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,6 @@ class Profile < ApplicationRecord
   include PhotoUploader.attachment(:photo) 
 
   belongs_to :user
-  has_one :address
+  has_one :address, dependent: :destroy
   accepts_nested_attributes_for :address
 end
